7a133321591a69350d81dbe17a08ef251c00af34
Anton <dober92@list.ru>
2020-10-08 16:42:38 +0300

    update

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from './Header.jsx';
 export { default as Categories } from './Categories.jsx';
+export { default as Sort } from './Sort.jsx';
 export { default as Footer } from './Footer.jsx';
